17f4e4447ee0b5e12f06761039d20f9ac82d8fa5
fishygaminglife-del <fishygaming.life@gmail.com>
2026-01-05 20:32:38 -0600

    Added assets, and started character.

---
+++ scripts/Player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -200.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = direction < 0
+
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
+	move_and_slide()
---
+++ scripts/test.gd
@@ -0,0 +1,8 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# do absolutley nothing
+	#I am makign stuff
+	pass
